c6c96d20a7ffab86e6f751ca419b6e8af70678d5
Fletcher Hardison <fhardison@@gmail.com>
2023-06-21 11:24:58 -0400

    making pwa

---
+++ sw.js
@@ -0,0 +1,19 @@
+ar staticCacheName = "pwa";
+ 
+self.addEventListener("install", function (e) {
+  e.waitUntil(
+    caches.open(staticCacheName).then(function (cache) {
+      return cache.addAll(["index.html", "./static/style.css", "./static/dict_data.js", "yew-app-6df...d54)bg.wasm", "yew-app-6df...3583ec8d54.js"]);
+    })
+  );
+});
+ 
+self.addEventListener("fetch", function (event) {
+  console.log(event.request.url);
+ 
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      return response || fetch(event.request);
+    })
+  );
+});
